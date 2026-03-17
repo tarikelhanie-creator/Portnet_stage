5a6ab627b58e73bfd3f0709c702ff83643540e82
tarikelhanie-creator <tarikelhanie@gmail.com>
2026-03-17 12:32:27 +0000

    added a register port

--- src/main/java/com/tarik/content_clander/controller/AuthController.java
+++ src/main/java/com/tarik/content_clander/controller/AuthController.java
@@ -2,7 +2,10 @@ package com.tarik.content_clander.controller;
 
 
 import com.tarik.content_clander.DTO.LoginRequestDTO;
+import com.tarik.content_clander.DTO.UserDTO;
+import com.tarik.content_clander.model.User;
 import com.tarik.content_clander.security.JwtUtil;
+import com.tarik.content_clander.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,6 +20,9 @@ public class AuthController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
 
     @Autowired
     private JwtUtil jwtUtil;
@@ -34,6 +40,16 @@ public class AuthController {
             return ResponseEntity.ok(token);
         }catch (AuthenticationException e){
             return  ResponseEntity.status(401).body("Inavlid username or password");
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody User user){
+        try{
+            UserDTO newuser = userService.createUser(user);
+            return ResponseEntity.status(201).body(newuser);
+        }catch(RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
